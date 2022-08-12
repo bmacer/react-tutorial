05e01f57cea4c059f93a910b04170d04ca337d0c
Brandon Macer <bobbysox322@gmail.com>
2022-08-12 13:50:06 -0400

    convert to functional (initial)

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -6,53 +6,41 @@ import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 
+const App = () => {
 
-class App extends Component {
-  //// Concept: LOCAL STATE and setState
-  // Best practice, use the same object structure when updating
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      query: "",
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
+  
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    if (searchFieldString != searchField) {
+      setSearchField(searchFieldString);
     }
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-        return {monsters: users};
-      }, () => {
-        // console.log(this.state);
-      }
-    ));
-  }
+  useEffect(() => {
+    setFilteredMonsters(monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    }));
+  },
+  [searchField]);
   
-  handleQueryChange = (event) => {
-    this.setState(() => {
-      return { query: event.target.value.toLocaleLowerCase() }
-    });
-  }
 
-  render() {
-    console.log("render() running...");
-
-    const { monsters, query } = this.state;
-    const { handleQueryChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(query);
-    });
-    
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox className="monsters-search-box" placeholder="Search Monsters" onChange={handleQueryChange} />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox className="monsters-search-box" placeholder="Search Monsters" onChange={onSearchChange} />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
 }
 
 export default App;
