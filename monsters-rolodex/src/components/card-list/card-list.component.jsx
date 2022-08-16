5115389111878a012a37c9547b377f99e2dbe584
Brandon Macer <bobbysox322@gmail.com>
2022-08-16 10:56:42 -0400

    add key

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ const CardList = ({ monsters }) => {
     return (
         <div className='card-list'>
             {monsters.map((monster) => {
-                return <Card monster={monster} />
+                return <Card key={monster.id} monster={monster} />
             })}
         </div>
     );
