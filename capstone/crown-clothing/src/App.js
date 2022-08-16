db840e6bd909486bfbdaffdd424eaf9b3157687e
Brandon Macer <bobbysox322@gmail.com>
2022-08-16 11:31:19 -0400

    scaffolding capstone

---
+++ capstone/crown-clothing/src/App.js
@@ -0,0 +1,49 @@
+const App = () => {
+  const categories = ['mens', 'womens'];
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <img />
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <img />
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Women's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <img />
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Men's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <img />
+    </div>
+      
+  );
+}
+
+export default App;
