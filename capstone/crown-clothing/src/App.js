ed0a1381e051789ffda39091bafe26e8b3220bab
Brandon Macer <bobbysox322@gmail.com>
2022-08-16 16:21:10 -0400

    initial routing

--- capstone/crown-clothing/src/App.js
+++ capstone/crown-clothing/src/App.js
@@ -1,38 +1,17 @@
-import Directory from './components/directory/directory.component';
+import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom';
+
+const Shop = () => (
+  <h1>Shop!</h1>
+)
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
   return (
-    <div className='container'>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path='/home' element={<Home />}>
+        <Route path='/home/shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
