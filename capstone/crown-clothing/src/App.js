bd76380a79409ce7e2a62c7cf8c142fa23e50e5d
Brandon Macer <bobbysox322@gmail.com>
2022-08-17 16:01:54 -0400

    initialize navbar

--- capstone/crown-clothing/src/App.js
+++ capstone/crown-clothing/src/App.js
@@ -1,15 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from 'react-router-dom';
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => (
-  <h1>Shop!</h1>
+  "Shop"
 )
-
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
-        <Route path='/home/shop' element={<Shop />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
       </Route>
     </Routes>
   );
