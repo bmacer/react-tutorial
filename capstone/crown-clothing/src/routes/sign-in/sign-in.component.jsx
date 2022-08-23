9f1d46efc4281a4339210f14032c9fa35395f33a
Brandon Macer <bobbysox322@gmail.com>
2022-08-22 21:26:00 -0400

    firebase google popup auth

--- capstone/crown-clothing/src/routes/sign-in/sign-in.component.jsx
+++ capstone/crown-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
-        console.log(response);
+        const _userDocRef = createUserDocumentFromAuth(response.user);
     }
     return (
         <div>
--- capstone/crown-clothing/src/utils/firebase/firebase.utils.js
+++ capstone/crown-clothing/src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import {
     GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCcInGjah607NIpe5eBF9sWfY3_RmXqUy8",
@@ -28,3 +35,21 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+    // console.log(userSnapshot);
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.error(`Error creating userDoc: ${error}`);
+        }
+    }
+    return userDocRef;
+}
