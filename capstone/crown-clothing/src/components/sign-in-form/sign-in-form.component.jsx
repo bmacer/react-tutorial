c919432153a0e0161193acb48f0359c5eb2fa6b1
Brandon Macer <bobbysox322@gmail.com>
2022-08-23 14:59:47 -0400

    finalizing authentication

---
+++ capstone/crown-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,82 @@
+import "./sign-in-form-styles.scss";
+
+import {
+  signInWithGooglePopup,
+  signInWithEmail,
+} from "../../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+const signInWithGoogle = async () => {
+  const { user } = await signInWithGooglePopup();
+  const userDocRef = await createUserDocumentFromAuth(user);
+  return userDocRef;
+};
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+    console.log(formFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await signInWithEmail(email, password);
+    } catch (error) {
+      console.log(error);
+      if (
+        error.code === "auth/user-not-found" ||
+        error.code === "auth/wrong-password"
+      ) {
+        alert("authorization!");
+      }
+    }
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>I already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          type="email"
+          required
+        />
+
+        <FormInput
+          label="Password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          type="password"
+          required
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" onClick={signInWithGoogle} buttonType="google">
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
--- capstone/crown-clothing/src/routes/navigation/navigation.component.jsx
+++ capstone/crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,25 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
-import './navigation.styles.scss';
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => (
-    <Fragment>
-        <div className='navigation'>
-            <Link className='logo-container' to='/'>
-                <CrownLogo className='logo' />
-            </Link>
-            <div className='nav-links-container'>
-                <Link className='nav-link' to='/shop'>Shop</Link> 
-                <Link className='nav-link' to='/sign-in'>Sign In</Link>
-            </div>
-        </div>
-        <Outlet />
-    </Fragment>
-  )
+  <Fragment>
+    <div className="navigation">
+      <Link className="logo-container" to="/">
+        <CrownLogo className="logo" />
+      </Link>
+      <div className="nav-links-container">
+        <Link className="nav-link" to="/shop">
+          Shop
+        </Link>
+        <Link className="nav-link" to="/auth">
+          Sign In
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+  </Fragment>
+);
 
 export default Navigation;
